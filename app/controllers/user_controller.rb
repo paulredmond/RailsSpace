5b3ba1082b69ad2677d598057ff922f2e2087e6b
paulredmond <paulrredmond@gmail.com>
2010-06-10 21:57:47 -0700

    Added user login. Left off on page #142

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,9 +9,28 @@ class UserController < ApplicationController
     if request.post? and params[:user]
       @user = User.new(params[:user])
       if @user.save
+        session[:user_id] = @user.id
         flash[:notice] = "User #{@user.screen_name} created!"
         redirect_to :action => 'index'
       end
     end
   end
+  
+  def login
+    @title = "Log in to RailsSpace"
+    if request.post? and params[:user]
+      @user = User.new(params[:user])
+      user = User.find_by_screen_name_and_password(@user.screen_name, @user.password)
+      if user
+        session[:user_id] = user.id
+        flash[:notice] = "User #{user.screen_name} logged in!"
+        redirect_to :action => 'index'
+      else
+        # Don't show the password in the view.
+        @user.password = nil
+        flash[:notice] = "Invalid screen name/password combination"
+      end
+    end
+  end
+  
 end
